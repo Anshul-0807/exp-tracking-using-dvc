98ae0e498a1ab52e37718c7c79b3086326966c4c
Anshul patle <patleanshul920@gmail.com>
2024-11-26 01:36:48 +0530

    commit changes

--- src/model_training.py
+++ src/model_training.py
@@ -24,10 +24,10 @@ y_pred = rf.predict(X_test)
 
 
 with Live(save_dvc_exp=True) as live:
-    live.log_metri('accuracy', accuracy_score(y_test, y_pred))
+    live.log_metric('accuracy', accuracy_score(y_test, y_pred))
     live.log_metric('precision', precision_score(y_test, y_pred))
     live.log_metric('recall', recall_score(y_test, y_pred))
     live.log_metric('f1_score', f1_score(y_test, y_pred))
 
-    live.log_params('n_estimator', n_estimators)
-    live.log_params('max_depth', max_depth)
+    live.log_param('n_estimator', n_estimators)
+    live.log_param('max_depth', max_depth)
